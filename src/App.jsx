3b69e037f10b63840d2fe4bd43480337e33cbf74
shoxjahon_asqarov <shohjahonasqarov.web@gmail.com>
2024-07-30 12:00:01 +0500

    merge kamron

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Shohjahon from './component/Shohjahon'
 import Kamron from './component/Kamron'
 import Xasan from './component/Xasan'
+import Navbar from './component/Navbar'
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Shohjahon />
       <Kamron />
       <Xasan />
