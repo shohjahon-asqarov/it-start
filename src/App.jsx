15269c26d59c55114656b483d34d1b07fb107a58
shoxjahon_asqarov <shohjahonasqarov.web@gmail.com>
2024-07-30 11:33:00 +0500

    add kamron

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Shohjahon from './component/Shohjahon'
+import Kamron from './component/Kamron'
 
 const App = () => {
   return (
     <div>
       <Shohjahon />
+      <Kamron />
     </div>
   )
 }
