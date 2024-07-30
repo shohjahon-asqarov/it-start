c40f6723d7d977bd67a47159706ed864eb1864ba
shoxjahon_asqarov <shohjahonasqarov.web@gmail.com>
2024-07-30 11:15:12 +0500

    add component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Shohjahon from './component/Shohjahon'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Shohjahon />
+    </div>
   )
 }
 
---
+++ src/component/Shohjahon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Shohjahon = () => {
+    return (
+        <div>
+            <h1 className='text-3xl text-center'>Shohjahon</h1>
+        </div>
+    )
+}
+
+export default Shohjahon
